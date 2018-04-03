15c342d86b96ac902d53f857aa213fcdd67639b1
Steveng2882 <31765885+Steveng2882@users.noreply.github.com>
2018-04-03 13:36:18 -0500

    Add files via upload

--- PHP/editMemory.php
+++ PHP/editMemory.php
@@ -4,7 +4,7 @@
   }
   include "database.php";
   $prodID = $_GET['prod'];
-  $sql = "SELECT PartID, Quanity, Brand, Type, BarCode, PartNumber from memory join bar_code on memory.BarCodeID = bar_code.BarCodeID join part_number on bar_code.BarCodeID = part_number.BarCodeID where PartID = ${prodID}";
+  $sql = "SELECT PartID, Quanity, IsNew, IsTested, Brand, Type, Rate, StandardName, ModuleName, IsLowVoltage, BarCode, PartNumber FROM memory join bar_code on memory.BarCodeID = bar_code.BarCodeID join part_number on bar_code.BarCodeID = part_number.BarCodeID where PartID = ${prodID}";
   $qry = mysqli_query($db, $sql);
  ?>
 <!DOCTYPE html>
@@ -19,28 +19,43 @@
     <form method="post" action="memory.php">
 
 
-    <table width="60%">
-	  <tr>
-	    <th>
-		Brand
-	    </th>
-		<th>
-		Type
-	    </th>
-		<th>
-		Bar Code
-	    </th>
-		<th>
+    <table width="100%">
+	<tr>
+      <th>
 		Quantity
-	    </th>
-	  </tr>
+      </th>
+      <th>
+        Is New
+      </th>
+      <th>
+        Is Tested
+      </th>
+	  <th>
+        Brand
+      </th>
+	  <th>
+		Type
+	  </th>
+	  <th>
+		Rate
+	  </th>
+	  <th>
+		Module Name
+	  </th>
+	  <th>
+		Is Low Voltage
+	  </th>
+	  <th>
+		Part Number
+	  </th>
+    </tr>
 
     <?php
       $rs = mysqli_fetch_array ($qry);
       extract($rs);
-      print "<td>${Brand}\t</td><td>${Type}\t</td><td>${BarCode}\t</td><td>
-      <input type='text' value='${Quanity}' name='qty'
-      </td>";
+      print "<td><input type='text' value='${Quanity}' name='qty'/></td><td><input type='text' value='${IsNew}' name='isnew' size='1'/></td><td><input type='text' value='${IsTested}' name='tested' size='1'/></td><td><input type='text' value ='${Brand}' name='brand' size='6'/>
+	  </td><td><input type='text' value='${Type}' name='type'/></td><td><input type='text' value='${Rate}' name='rate' size='10'/></td>
+	  <td><input type='text' value='${ModuleName}' name='mname'/></td><td><input type='text' value='${IsLowVoltage}' name='voltage' size='1'/></td><td><input type='text' value='${PartNumber}' name='pnum'/></td>";
 
 
     ?>
