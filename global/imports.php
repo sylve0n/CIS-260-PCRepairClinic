9e78b32f025fe37bfdf7e8ad32386e2f82941b9c
Unknown <sartinh@otc.edu>
2018-05-10 12:30:51 -0500

    Added Icon

--- global/imports.php
+++ global/imports.php
@@ -6,7 +6,7 @@
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.12/css/all.css" integrity="sha384-G0fIWCsCzJIMAVNQPfjH08cyYaUtMwjJwqiRKxxE/rx96Uroj1BtIQ6MLJuheaO9" crossorigin="anonymous">
 
 <link href="https://fonts.googleapis.com/css?family=Libre+Barcode+128+Text" rel="stylesheet">
-
+<link rel="shortcut icon" type="image/x-icon" href="global/icon.ico"/>
 
 <script href="global/materialize/js/materialize.js"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
