2ab86ba3aea361d357a7defe1e849f58ae87d75d
as0354967@otc.edu <as0354967@otc.edu>
2018-05-11 01:58:47 -0500

    Modified to work on AWS Server
    Had to modify to for AWS

--- clinichome.php
+++ clinichome.php
@@ -40,6 +40,6 @@
 
     <!--Import jQuery before materialize.js-->
     <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
-    <script type="text/javascript" src="materialize/js/materialize.min.js"></script>
+    <script type="text/javascript" src="global/materialize/js/materialize.min.js"></script>
   </body>
 </html>
